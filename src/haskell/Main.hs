db6aad4cc3a5b727a3af76f51443bc60cf1b0bc4
Jean-Baptiste Potonnier <jb@dune.io>
2014-10-19 02:48:40 +0200

    refactor

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -2,31 +2,29 @@
 
 module Main where
 
-import           Control.Monad.Reader (ReaderT)
-import           Control.Monad.State.Strict (StateT)
 import           Data.Aeson (FromJSON, ToJSON)
 import           GHC.Generics (Generic)
 import           Network.EngineIO.Snap (snapAPI)
-import           Network.SocketIO (initialize, RoutingTable, Socket)
+import           Network.SocketIO (initialize)
 import qualified Network.SocketIO as Socketio
 import           Snap (Snap, route, quickHttpServe, dir)
 import Snap.Util.FileServe (serveDirectory)
 import Control.Applicative ((<|>))
 import qualified Data.Text as Text
 
-data Message = Message {messageText :: Text.Text} deriving (Generic, Show)
+data Message = Message {messageText :: Text.Text} deriving Generic
 instance FromJSON Message
 
 data Response = Response {responseText :: Text.Text} deriving Generic
 instance ToJSON Response
 
-mkRoutes ::  StateT RoutingTable (ReaderT Socket Snap) ()
-mkRoutes = Socketio.on "message" $ \m ->
-  Socketio.emit "response" $ Response (messageText m)
+mkRoutes :: IO (Snap ())
+mkRoutes = initialize snapAPI $
+  Socketio.on "message" $ \m -> Socketio.emit "response" $ Response (messageText m)
 
 main :: IO ()
 main = do
-  handler <- initialize snapAPI mkRoutes
+  handler <- mkRoutes
   quickHttpServe $ 
     route [("/socket.io", handler)] <|>
     dir "static" (serveDirectory "src/static/")
